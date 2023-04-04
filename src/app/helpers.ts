5822b84c24dc63b8c1966117eb7c066c018f685d
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2023-04-04 12:19:10 -0500

    docs: add the intent of `getFormControlStatus` helper function

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,6 +1,10 @@
 import { FormControl } from '@angular/forms';
 import { map } from 'rxjs';
 
+/**
+ * This is intended to be used for computing `aria-invalid` input attributes
+ * and match PicoCSS input state styles
+ */
 export function getFormControlStatus(control: FormControl) {
   return control.statusChanges.pipe(
     map((status) => (status === 'INVALID' ? 'true' : ''))
