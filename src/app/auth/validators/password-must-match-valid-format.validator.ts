404d98ff089593b1ab83e9c7de6cc512dab764ca
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2023-04-04 09:51:36 -0500

    fix: add fallback string to password format validator

--- src/app/auth/validators/password-must-match-valid-format.validator.ts
+++ src/app/auth/validators/password-must-match-valid-format.validator.ts
@@ -1,10 +1,15 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
 import { passwordFormat } from '../constants/password-format';
 
 export const passwordMustMatchValidFormat: ValidatorFn = (
   control: AbstractControl
 ): null | ValidationErrors => {
-  const value = control.value as string;
+  const value = (control as FormControl<string | null>).value ?? '';
   if (value.length === 0) return null;
 
   return passwordFormat.test(value)
